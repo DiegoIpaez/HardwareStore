be49fa286885d0a31ddd1bf907e525a1f403e627
Enzo Imanol Paz <enzoimanolpaz@gmail.com>
2022-04-15 13:06:03 -0300

    creación de la entidad Categoria

---
+++ src/main/java/com/egg/proyectospring/entidades/Categoria.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package com.egg.proyectospring.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+}
