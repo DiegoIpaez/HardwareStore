628493b3c1dcb7a22601f404b56c59c4d814dd5e
Enzo Imanol Paz <enzoimanolpaz@gmail.com>
2022-04-17 20:24:29 -0300

    agregado de etiqueta @Data a la entidad Categoria. CategoriaController funcionando

--- src/main/java/com/egg/proyectospring/controladores/CategoriaController.java
+++ src/main/java/com/egg/proyectospring/controladores/CategoriaController.java
@@ -3,11 +3,82 @@
  */
 package com.egg.proyectospring.controladores;
 
+import com.egg.proyectospring.entidades.Categoria;
+import com.egg.proyectospring.servicios.CategoriaServicio;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/categoria")
 public class CategoriaController {
+    
+    @Autowired
+    private CategoriaServicio categoriaServicio;
+    
+    @GetMapping("/form")
+    public String formulario(Model modelo) {
+        Categoria categoria = new Categoria();
+        modelo.addAttribute("categoria", categoria);
+        return "formulario-categoria";
+    }
+    
+    @PostMapping("/save")
+    public String form(@ModelAttribute("categoria") Categoria categoria, Model modelo) {
+        
+        try {
+            categoriaServicio.guardarCategoria(categoria);
+            
+            modelo.addAttribute("success", "Se ingresó una categoría");
+            
+            return "formulario-categoria";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            modelo.addAttribute("error", ex.getMessage());
+            return "formulario-categoria";
+        }
+    }
+    
+    @GetMapping("/list")
+    public String lista(Model modelo) {
+        
+        List<Categoria> categorias = categoriaServicio.listAll();
+        modelo.addAttribute("listaDeCategorias", categorias);
+        return "lista-categoria";
+    }
+    
+    @GetMapping("/baja") 
+    public String darDeBaja(@RequestParam("id") String id) {
+        
+        try {
+            categoriaServicio.darDeBaja(id);
+                     
+            return "redirect:/categoria/list";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+           return "redirect:/categoria/list";
+        }
+    }
+    
+    @GetMapping("/alta")
+    public String darDeAlta(@RequestParam("id") String id) {
+        
+        try {
+            categoriaServicio.darDeAlta(id);
+            
+            return "redirect:/categoria/list";
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "redirect:/categoria/list";
+        }
+    }
     
 }
--- src/main/java/com/egg/proyectospring/entidades/Categoria.java
+++ src/main/java/com/egg/proyectospring/entidades/Categoria.java
@@ -6,9 +6,11 @@ package com.egg.proyectospring.entidades;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Data
 public class Categoria {
     
     @Id
@@ -19,29 +21,7 @@ public class Categoria {
     private String nombre;
     private Boolean alta;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Boolean getAlta() {
-        return alta;
-    }
-
-    public void setAlta(Boolean alta) {
-        this.alta = alta;
-    }
     
     
 }
