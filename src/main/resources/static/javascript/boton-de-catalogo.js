e056632c439b2ae02cfd26a6da227687e6772c69
DiegoIpaez <diegokpo1313@gmail.com>
2022-05-06 16:30:23 -0300

    catalogo añadido al navbar

---
+++ src/main/resources/static/javascript/boton-de-catalogo.js
@@ -0,0 +1,28 @@
+const urlApi = "http://localhost:8080/api";
+            const ulC = document.getElementById("categorias");
+            const liC = document.createElement("li");
+            const ulM = document.getElementById("marcas");
+            const liM = document.createElement("li");
+            fetch(urlApi+"/categorias")
+            .then((response) => response.json())
+            .then((data) => {
+            let datos = "";
+            for (let i = 0; i < data.length; i++) {
+            datos += `<li><a class="dropdown-item" href="#">${data[i].nombre}</a></li>`;
+            }
+
+            liC.innerHTML = datos;
+            ulC.appendChild(liC);
+            });
+
+            fetch(urlApi+"/marcas")
+            .then((response) => response.json())
+            .then((data) => {
+            let datos = "";
+            for (let i = 0; i < data.length; i++) {
+            datos += `<li><a class="dropdown-item" href="#">${data[i].nombre}</a></li>`;
+            }
+
+            liM.innerHTML = datos;
+            ulM.appendChild(liM);
+            });
