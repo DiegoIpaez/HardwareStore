4e06a2d72bb8d91e19cded3c4ac582a34437c619
DiegoIpaez <diegokpo1313@gmail.com>
2022-05-07 16:46:13 -0300

    buscador added in producto-todos

--- src/main/java/com/egg/proyectospring/controladores/MainController.java
+++ src/main/java/com/egg/proyectospring/controladores/MainController.java
@@ -1,18 +1,9 @@
 package com.egg.proyectospring.controladores;
 
-import com.egg.proyectospring.entidades.Categoria;
-import com.egg.proyectospring.entidades.Marca;
 import com.egg.proyectospring.entidades.Producto;
-import com.egg.proyectospring.servicios.CategoriaServicio;
-import com.egg.proyectospring.servicios.MarcaServicio;
 import com.egg.proyectospring.servicios.ProductoServicio;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,26 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
-    
+
     @Autowired
     private ProductoServicio productoServicio;
-    @Autowired
-    private CategoriaServicio categoriaServicio;
-    @Autowired
-    private MarcaServicio marcaServicio;
-    
-     @GetMapping("")
-    public String index(Model model){
-        List<Categoria> categorias = categoriaServicio.categoriasConAlta();
-        List<Marca> marcas = marcaServicio.listarMarcas();
+
+    @GetMapping("")
+    public String index(Model model) {
         List<Producto> productos = productoServicio.productosLimitados();
-       
-        model.addAttribute("categorias", categorias);
-        model.addAttribute("marcas", marcas);
+
         model.addAttribute("productos", productos);
-        
-        
-    return "index";
+        return "index";
     }
-    
+
 }
--- src/main/java/com/egg/proyectospring/controladores/ProductoController.java
+++ src/main/java/com/egg/proyectospring/controladores/ProductoController.java
@@ -7,9 +7,8 @@ import com.egg.proyectospring.entidades.Producto;
 import com.egg.proyectospring.servicios.CategoriaServicio;
 import com.egg.proyectospring.servicios.MarcaServicio;
 import com.egg.proyectospring.servicios.ProductoServicio;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -196,29 +195,17 @@ public class ProductoController {
         return "formulario-producto";
     }
 
-    @GetMapping("/buscar")
-    public String buscarProducto(Model model) {
-        Producto producto = new Producto();
-        model.addAttribute("producto", producto);
-         List<Producto> productosBuscados = productoServicio.listarProductos();
-         model.addAttribute("productosBuscados", productosBuscados);
-         
-        return "buscador-productos";
-    }
-
     @GetMapping("/buscarProducto")
     public String listarProductos(Model modelo, @RequestParam("nombrep") String nombre) {
         List<Producto> productos;
         try {
             productos = productoServicio.buscarProducto(nombre);
-            modelo.addAttribute("productos", productos);
+            modelo.addAttribute("productosBuscados", productos);
         } catch (Exception ex) {
             ex.printStackTrace();
             modelo.addAttribute("error", ex.getMessage());
-        }
-
-        
-        return "buscador-productos";
+        }      
+        return "producto-todos";
     }
     
     @GetMapping("/productos")
@@ -230,6 +217,8 @@ public class ProductoController {
             List<Integer> paginas = IntStream.rangeClosed(1, totalDePaginas).boxed().collect(Collectors.toList());
             model.addAttribute("paginas", paginas);
         }
+        List<Producto> p = new ArrayList<>();
+        model.addAttribute("productosBuscados", p);
         model.addAttribute("productos", productos);
         model.addAttribute("actual", page);
         model.addAttribute("siguiente", page+1);
